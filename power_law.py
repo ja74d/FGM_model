0dc4a12316a792c8cb1081c822fc5c676079ebc9
Javad Pournosrat <Pournosrat.javad@gmail.com>
2024-08-19 23:05:38 +0330

    P(T dependent matrial proprties)

--- power_law.py
+++ power_law.py
@@ -12,8 +12,22 @@ Em = 70e+09
 
 nu = 0.3
 
+#Tempreture change trugh the thikness
+T = 1
+
+#Tempreture Dependent Matrial Properties
+def TDP(P0, P_1, P1, P2, P3):
+    P = P0*( P_1*(T**-1) +1 + P1*T + P2*(T**2) +P3*(T**3) )
+    return P
+#print(TDP(244.27e+09, 0, -1.371e-03, 1.214e-06, -3.681e-10))
+
+#Simple Power law
 def powerlaw(z, n, pc, pm, h):
+    #in case of no change in matrial properties with T change
     return (pc-pm)*(0.5+z/h)**n + pm
+    #in case of material properties dependent on temprucure change
+    #return (TDP(0, 0, 0, 0, 0) - TDP(0, 0, 0, 0, 0))*(0.5+z/h)**n + TDP(0, 0, 0, 0, 0)
+
 
 z_values = np.linspace(-h/2, h/2, 500)
 
@@ -49,14 +63,11 @@ Q[0, 1] = Q12 = Q[1, 0]
 Q[2, 2] = Q44
 Q[3, 3] = Q55
 Q[4, 4] = Q66
-
 #print(Q)
 
 #e (diffrence between Mid-plane and physical neutral plane)
-
 z, n = symbols('z n')
 E = (Ec-Em)*((z/h)+0.5)**n + Em
 
 e = ( integrate(z*E, (z, -h/2, h/2)) ) / (integrate(E, (z, -h/2, h/2)))
-
 #print(e)
